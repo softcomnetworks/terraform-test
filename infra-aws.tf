fbdf2588c2d0731ee3d825bbd383e882aa3eff0d
Ashfaq Ahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2020-10-15 00:03:35 +0100

    Destroy VMs

--- infra-aws.tf
+++ infra-aws.tf
@@ -15,17 +15,17 @@ provider "aws" {
     
 }
 
-resource "aws_instance" "App1" {
-    instance_type = "t2.micro"
-    ami = "ami-05c424d59413a2876"
-    key_name = "terraform-test-machines"
-}
+# resource "aws_instance" "App1" {
+#     instance_type = "t2.micro"
+#     ami = "ami-05c424d59413a2876"
+#     key_name = "terraform-test-machines"
+# }
 
-resource "aws_instance" "App2" {
-    instance_type = "t2.micro"
-    ami = "ami-05c424d59413a2876"
-    key_name = "terraform-test-machines"
-    tags = {
-        Name = "App2Machine"
-    }
-}
+# resource "aws_instance" "App2" {
+#     instance_type = "t2.micro"
+#     ami = "ami-05c424d59413a2876"
+#     key_name = "terraform-test-machines"
+#     tags = {
+#         Name = "App2Machine"
+#     }
+# }
