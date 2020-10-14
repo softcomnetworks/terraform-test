ac724291bd3f13424d73ef6550574ccc9f7b7fb4
geektype <abdullah@softcomnetworks.com>
2020-10-14 23:39:32 +0100

    Update infra-aws.tf

--- infra-aws.tf
+++ infra-aws.tf
@@ -19,6 +19,9 @@ resource "aws_instance" "App1" {
     instance_type = "t2.micro"
     ami = "ami-05c424d59413a2876"
     key_name = "terraform-test-machines"
+    tags = {
+        Name = "App1Machine"    
+    }
 }
 
 resource "aws_instance" "App2" {
