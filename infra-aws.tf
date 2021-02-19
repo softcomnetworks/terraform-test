16172d5635b6346e50dd9fe25b37bab7b7403411
RIashfaqahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-02-19 11:39:30 +0000

    Update infra-aws.tf

--- infra-aws.tf
+++ infra-aws.tf
@@ -15,14 +15,14 @@ provider "aws" {
     
 }
 
-# resource "aws_instance" "App1" {
-#     instance_type = "t2.micro"
-#     ami = "ami-05c424d59413a2876"
-#     tags = {
-#         Name = "App1Machine"    
-#     }
-#     key_name = "terraform-ec2"
-# }
+ resource "aws_instance" "App1" {
+     instance_type = "t2.micro"
+     ami = "ami-05c424d59413a2876"
+     tags = {
+         Name = "App1Machine"    
+     }
+     key_name = "terraform-ec2"
+ }
 
 # resource "aws_instance" "App2" {
 #     instance_type = "t2.micro"
