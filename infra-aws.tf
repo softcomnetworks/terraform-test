b2511fd16d9e0b20f919bd1b6d53fba34aa0e69e
RIashfaqahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-02-19 11:19:45 +0000

    Update infra-aws.tf

--- infra-aws.tf
+++ infra-aws.tf
@@ -32,3 +32,15 @@ provider "aws" {
 #     }
 #     key_name = "terraform-ec2"
 # }
+
+resource "aws_s3_bucket" "onebucket" {
+   bucket = "testing-s3-with-terraform"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "Bucket1"
+     Environment = "Test"
+   }
+}
