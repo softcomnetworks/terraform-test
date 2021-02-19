c7ffb85968360fd9cf1e2f963b841c5e6c74575d
RIashfaqahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-02-19 12:45:42 +0000

    Update infra-aws.tf

--- infra-aws.tf
+++ infra-aws.tf
@@ -15,14 +15,14 @@ provider "aws" {
     
 }
 
- resource "aws_instance" "App01" {
-     instance_type = "t2.micro"
-     ami = "ami-05c424d59413a2876"
-     tags = {
-         Name = "App1Machine"    
-     }
-     key_name = "terraform-ec2"
- }
+# resource "aws_instance" "App01" {
+#     instance_type = "t2.micro"
+#     ami = "ami-05c424d59413a2876"
+#     tags = {
+#         Name = "App1Machine"    
+#     }
+#     key_name = "terraform-ec2"
+# }
 
 # resource "aws_instance" "App2" {
 #     instance_type = "t2.micro"
@@ -33,29 +33,29 @@ provider "aws" {
 #     key_name = "terraform-ec2"
 # }
 
-resource "aws_s3_bucket" "starbucket" {
-   bucket = "mybucketokirjt"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   tags = {
-     Name = "Bucket1"
-     Environment = "Test"
-   }
-}
+#resource "aws_s3_bucket" "starbucket" {
+#   bucket = "mybucketokirjt"
+#   acl = "private"
+#   versioning {
+#      enabled = true
+#   }
+#   tags = {
+#     Name = "Bucket1"
+#     Environment = "Test"
+#   }
+#}
 
 
-variable "s3_bucket_name1" {
-   type = "list"
-   default = ["terr-ashfaq-buc-1", "terr-ashfaq-buc-2", "terr-ashfaq-buc-3"]
-}
-resource "aws_s3_bucket" "henrys_bucket_jjuue" {
-   count = "${length(var.s3_bucket_name1)}"
-   bucket = "${var.s3_bucket_name1[count.index]}"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   force_destroy = "true"
-}
+#variable "s3_bucket_name1" {
+#   type = "list"
+#   default = ["terr-ashfaq-buc-1", "terr-ashfaq-buc-2", "terr-ashfaq-buc-3"]
+#}
+#resource "aws_s3_bucket" "henrys_bucket_jjuue" {
+#   count = "${length(var.s3_bucket_name1)}"
+#   bucket = "${var.s3_bucket_name1[count.index]}"
+#   acl = "private"
+#   versioning {
+#      enabled = true
+#   }
+#   force_destroy = "true"
+#}
