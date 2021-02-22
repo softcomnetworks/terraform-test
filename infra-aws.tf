c9c9e8ea545fd78a0979763a69a1b70dbe8c2565
RIashfaqahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-02-22 15:47:46 +0000

    Update infra-aws.tf

--- infra-aws.tf
+++ infra-aws.tf
@@ -78,7 +78,7 @@ variable "environment_id" {
 variable "vip_bucket_retention_period" {
   type = number
   description = "desc2"
-  default = 7  
+  default = 3  
 }
 
 # S3 bucket for storing extracts as cache
